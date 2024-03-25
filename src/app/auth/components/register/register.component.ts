0e5c8d98112a9667bbaf9736e0a3ba828df81935
BCullenward <59399934+BCullenward@users.noreply.github.com>
2024-03-25 09:26:32 -0500

    starting to add selectors

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -5,6 +5,8 @@ import { Store } from '@ngrx/store';
 import { register } from '../../store/actions';
 import { RegisterRequestInterface } from '../../types/registerRequest.interface';
 import { RouterLink } from '@angular/router';
+import { selectIsSubmitting } from '../../store/selectors';
+import { AuthStateInterface } from '../../types/authState.interface';
 
 @Component({
   imports: [ReactiveFormsModule, RouterLink],
@@ -18,8 +20,12 @@ export class RegisterComponent {
     email: ['', Validators.required],
     password: ['', Validators.required],
   });
+  isSubmitting$ = this.store.select(selectIsSubmitting);
 
-  constructor(private fb: FormBuilder, private store: Store) {}
+  constructor(
+    private fb: FormBuilder,
+    private store: Store<{ auth: AuthStateInterface }>
+  ) {}
 
   onSubmit() {
     console.log('form', this.form.getRawValue());
